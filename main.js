1f860f832839ac0a6c2b17aacd9bab51185d2ea6
yonathan <yonathan.piat@gmail.com>
2025-04-08 18:02:53 +0200

    Configuration WebSocket server address.

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import { createBoard, playMove } from "./connect4.js";
 
+
+function getWebSocketServer() {
+  if (window.location.host === "python-websockets.github.io") {
+    return "wss://cooperative-domeniga-yona-b4f4c1ce.koyeb.app/";
+  } else if (window.location.host === "localhost:8000") {
+    return "ws://localhost:8001/";
+  } else {
+    throw new Error(`Unsupported host: ${window.location.host}`);
+  }
+}
+
 function initGame(websocket) {
   websocket.addEventListener("open", () => {
     // Send an "init" event according to who is connecting.
@@ -76,7 +87,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const board = document.querySelector(".board");
   createBoard(board);
   // Open the WebSocket connection and register event handlers.
-  const websocket = new WebSocket("ws://localhost:8001/");
+  const websocket = new WebSocket(getWebSocketServer());
   initGame(websocket);
   receiveMoves(board, websocket);
   sendMoves(board, websocket);
